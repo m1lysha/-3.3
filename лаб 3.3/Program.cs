687ab8f4669366d70f414f79109bb6b3f60c233f
RobotComp.ru <RobotComp.ru@DESKTOP-94TEC7C>
2024-11-07 22:07:41 +0300

    Добавьте файлы проекта.

---
+++ лаб 3.3/Program.cs
@@ -0,0 +1,25 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Console.Write("Введите значение x (в радианах): ");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите количество членов n: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        
+        double sum = 0.0;
+        for (int i = 1; i <= n; i++)
+        {
+            
+            double term = Math.Pow(-1, i - 1) * Math.Cos(i * x) / (i * i);
+            sum += term;
+        }
+
+       
+        Console.WriteLine($"Сумма ряда S(x) для n={n} и x={x} равна: {sum}");
+    }
+}
